ac6f92e6ef25e052669b4223f75b487f907bb4c3
raisultan <ki.xbozz@gmail.com>
2021-06-14 00:58:04 +0700

    orm layer for verifications

---
+++ moderation/app/crud/verification.py
@@ -0,0 +1,42 @@
+from typing import Union, List, Type
+from sqlalchemy.orm import Session
+
+from app.crud.base import CRUDBase
+from app.models import verification as models
+from app.schemas import verification as schemas
+from app.db import Base
+
+
+class CRUDLEVerificationRequest(
+    CRUDBase[
+        models.LegalEntityCreatorVerificationRequest,
+        schemas.LegalEntityCreatorVerificationRequest,
+        schemas.LegalEntityCreatorVerificationRequest,
+    ]
+):
+    ...
+
+
+class CRUDNPVerificationRequest(
+    CRUDBase[
+        models.NaturalPersonCreatorVerificationRequest,
+        schemas.NaturalPersonCreatorVerificationRequest,
+        schemas.NaturalPersonCreatorVerificationRequest,
+    ]
+):
+    ...
+
+
+GenericVerificationRequest = Type[
+    Union[
+        models.LegalEntityCreatorVerificationRequest,
+        models.NaturalPersonCreatorVerificationRequest,
+    ]
+]
+
+class CRUDVerificationRequest(Base):
+    def __init__(self, model: GenericVerificationRequest):
+        self.model = model
+
+    def read_multi(self, db: Session, *, skip: int = 0, limit: int = 100) -> List[GenericVerificationRequest]:
+        return db.query(self.model).offset(skip).limit(limit).order_by(self.model.created_at)
