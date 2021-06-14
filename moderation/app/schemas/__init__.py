22cb852a6cb9fc4c97b045b6c54f147023797a40
raisultan <ki.xbozz@gmail.com>
2021-06-14 16:15:00 +0700

    generic schema added

--- moderation/app/schemas/__init__.py
+++ moderation/app/schemas/__init__.py
@@ -1,4 +1,5 @@
 from .verification import (
+    GenericCreatorVerificationRequest,
     LegalEntityCreatorVerificationRequest,
     LegalEntityCreatorVerificationRequestDB,
     NaturalPersonCreatorVerificationRequest,
