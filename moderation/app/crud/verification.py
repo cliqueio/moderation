ee5fc4327aad3341ef722ef53d9b78d380d8c407
raisultan <ki.xbozz@gmail.com>
2021-06-14 16:14:51 +0700

    generic manager fixed

--- moderation/app/crud/verification.py
+++ moderation/app/crud/verification.py
@@ -3,7 +3,6 @@ from typing import List, Union
 from sqlalchemy.orm import Session
 
 from app.crud.base import CRUDBase
-from app.db import Base
 from app.models import verification as models
 from app.schemas import verification as schemas
 
@@ -28,16 +27,24 @@ class CRUDNPVerificationRequest(
     ...
 
 
-GenericVerificationRequest = Union[
-    models.LegalEntityCreatorVerificationRequest,
-    models.NaturalPersonCreatorVerificationRequest,
-]
-
 class CRUDVerificationRequest:
-    def list(self, db: Session, *, skip: int = 0, limit: int = 100) -> List[GenericVerificationRequest]:
-        le_requests = db.query(models.LegalEntityCreatorVerificationRequest).offset(skip).limit(limit)
-        np_requests = db.query(models.NaturalPersonCreatorVerificationRequest).offset(skip).limit(limit)
-        return le_requests.union(np_requests)
+    def list(
+        self,
+        db: Session,
+        *,
+        skip: int = 0,
+        limit: int = 100,
+    ) -> List[
+            Union[
+            models.LegalEntityCreatorVerificationRequest,
+            models.NaturalPersonCreatorVerificationRequest,
+        ]
+    ]:
+        requests = db.query(
+            models.LegalEntityCreatorVerificationRequest,
+            models.NaturalPersonCreatorVerificationRequest,
+        ).offset(skip).limit(limit).all()
+        return requests
 
 
 le_verification_request = CRUDNPVerificationRequest(models.LegalEntityCreatorVerificationRequest)
